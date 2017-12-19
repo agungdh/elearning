f7c10cb659ded63573619fe7327e740b339fc6b8
AgungDH <agungdh@live.com>
2017-12-19 16:11:48 +0700

    ganti config

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/pum_elearning2/';
-// $config['base_url'] = 'http://192.168.43.24/pum_elearning/';
+// $config['base_url'] = 'http://192.168.43.51/pum_elearning/';
 
 /*
 |--------------------------------------------------------------------------
